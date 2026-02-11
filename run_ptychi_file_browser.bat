62df45f79cb6831f2ed99a2593a382a26fafb5b8
O'Neal <oneal@anl.gov>
2026-02-11 09:24:42 -0600

    Remove ignored files

--- run_ptychi_file_browser.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM === Change this to your conda installation path ===
-CALL "C:\Users\oneal\AppData\Local\anaconda3\Scripts\activate.bat"
-
-REM === Run your python script ===
-python "C:\Users\oneal\Desktop\code\ptychi_file_browser\ptychi_file_browser.py"
